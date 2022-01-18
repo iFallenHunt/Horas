3463b9e39665fe5a0d7d8d27e8b6d32fd64c0448
TI <TI@TI_VISUAL>
2022-01-18 16:50:03 -0300

    Adicionar arquivos de projeto.

---
+++ Horas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            // se o comando apos a condicional for simples, ou seja, apenas uma condição, nao é necessario as chaves "{}"
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa Noite!");
+            }
+        }
+    }
+}
